099e03f0ce200039fd3f209c3f54cd7750c75f80
PhillipWhiter <boring.someone@gmail.com>
2022-01-11 21:18:02 -0500

    comment out batteryinfo

--- src/main/cpp/SubsystemManager.cpp
+++ src/main/cpp/SubsystemManager.cpp
@@ -3,6 +3,7 @@
 #include <exception>
 #include <frc/Errors.h>
 #include "rclcpp/rclcpp.hpp"
+#include <frc/smartdashboard/SmartDashboard.h>
 
 using std::placeholders::_1;
 using std::placeholders::_2;
@@ -17,7 +18,7 @@ namespace robot
     {
         sysReset = this->create_service<std_srvs::srv::Trigger>("/sys/reset", std::bind(&SubsystemManager::serviceReset, this, _1, _2));
         sysDebug = this->create_service<std_srvs::srv::SetBool>("/sys/debug", std::bind(&SubsystemManager::serviceDebug, this, _1, _2));
-        battery = robot::Battery();
+        //battery = robot::Battery();
     }
 
     void SubsystemManager::registerSubsystems(std::vector<std::shared_ptr<Subsystem>> subsystems)
@@ -127,7 +128,7 @@ namespace robot
 
             rclcpp::spin_some(this->shared_from_this());
             //battery stuff
-            frc::SmartDashboard::PutNumber("Drive/Pose/Theta", battery.GetPowerUsage());
+            //frc::SmartDashboard::PutNumber("Drive/Pose/Theta", battery.GetPowerUsage());
         }
         catch (const std::exception &e)
         {
@@ -158,7 +159,7 @@ namespace robot
             frc::ReportError(frc::err::Error, "SubsystemManager.cpp", 135, "enabledLoop()", "Looper Thread died with unknown exception");
         }
         //battery stuff
-        frc::SmartDashboard::PutNumber("Drive/Pose/Theta", battery.GetPowerUsage());
+        //frc::SmartDashboard::PutNumber("Drive/Pose/Theta", battery.GetPowerUsage());
     }
 
 } // namespace robot
